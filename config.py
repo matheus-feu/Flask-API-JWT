8d7bc11e3e4400d7b263aa2334e06d6c72698acc
Matheus Feu <101346300+Havokzbr@users.noreply.github.com>
2023-03-24 20:13:32 -0300

    fix: configuração banco de dados

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
 from app.helpers import generate_hash_new
+import os
+
+db_host = os.environ.get('DB_HOST', 'localhost')
+db_port = os.environ.get('DB_PORT', '3309')
+
+SQLALCHEMY_DATABASE_URI = f'mysql://admin:admin@{db_host}:{db_port}/flaskapp'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+generate_hash_new()
+TOKEN_EXPIRATION = 10
+SECRET_KEY = open('hash.txt', 'r').read()
+DEBUG = True
 
 
-class Config(object):
-    """Toda configuração do Flask."""
-    SQLALCHEMY_DATABASE_URI = 'mysql://flask:admin@localhost:3309/flaskapp'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    generate_hash_new()
-    TOKEN_EXPIRATION = 10
-    SECRET_KEY = open('hash.txt', 'r').read()
-    DEBUG = True
+
+
