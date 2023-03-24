64a21592dd13a0bb602a884e6ad7dcdd95ddd98b
Matheus Feu <101346300+Havokzbr@users.noreply.github.com>
2023-03-24 20:13:57 -0300

    feat: documentação swagger

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,22 @@
+from flasgger import Swagger
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
-from config import Config
+from app.docs.swagger import swagger_config, template
 
 app = Flask(__name__)
-app.config.from_object(Config)
-app.app_context().push()
+app.config.from_object('config')
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+swagger = Swagger(app, config=swagger_config, template=template)
+
+with app.app_context():
+    from app.models import user
+
+    db.create_all()
 
 # Rotas da aplicação
 from app.routes import user_routes
